3718c08c11f1ddadc120c124f5f47f488ae4ac90
David Anderson <david.anderson@networksecure.com.br>
2026-05-22 10:10:49 -0300

    add log

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/DavidAndersonAR/gopportunities.git/router"
+import (
+	
+
+	"github.com/DavidAndersonAR/gopportunities.git/config"
+	"github.com/DavidAndersonAR/gopportunities.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+
 	// Initialize router
 	router.Initialize()
 
